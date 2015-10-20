4217c3cd40b501eeaf6f6d2831216cdcc6949e4c
Sergej Derjabkin <sergej@derjabkin.de>
2015-10-20 11:59:16 +0200

    - Report Parameter Speichern

--- ReportsV1.Module/BusinessObjects/PersonReportParametersObject.cs
+++ ReportsV1.Module/BusinessObjects/PersonReportParametersObject.cs
@@ -10,12 +10,31 @@ namespace ReportsV1.Module.BusinessObjects
 {
     public class PersonReportParametersObject : ReportParametersObjectBase
     {
+        const string theUltimativeUserName = "Admin";
+
         private readonly IObjectSpaceCreator provider;
         public PersonReportParametersObject(IObjectSpaceCreator provider)
             : base(provider)
         {
             this.provider = provider;
+
+            using (var objectSpace = provider.CreateObjectSpace(typeof(Person)))
+            {
+                var p = FindParameters(objectSpace);
+
+                if (p != null)
+                    Name = p.Name;
+
+            }
         }
+
+        private static PersonSearchParameters FindParameters(DevExpress.ExpressApp.IObjectSpace objectSpace)
+        {
+            var p = objectSpace.FindObject<PersonSearchParameters>(
+                new BinaryOperator("UserName", theUltimativeUserName));
+            return p;
+        }
+
 
         public string Name { get; set; }
 
@@ -24,10 +43,25 @@ namespace ReportsV1.Module.BusinessObjects
             return provider.CreateObjectSpace(typeof(Person));
         }
 
+        private void SaveCriteria()
+        {
+            using(var objectSpace = CreateObjectSpace())
+            {
+                var p = FindParameters(objectSpace);
+                if (p == null)
+                {
+                    p = objectSpace.CreateObject<PersonSearchParameters>();
+                    p.UserName = theUltimativeUserName;
+                }
+
+                p.Name = Name;
+                objectSpace.CommitChanges();
+            }
+        }
         public override DevExpress.Data.Filtering.CriteriaOperator GetCriteria()
         {
             //CriteriaOperator.Parse("FirstName" LIKE ? OR LastName LIKE ?")
-
+            SaveCriteria();
             string name = Name + '%';
             return CriteriaOperator.Or(
                 new BinaryOperator("FirstName", name, BinaryOperatorType.Like),
---
+++ ReportsV1.Module/BusinessObjects/PersonSearchParameters.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportsV1.Module.BusinessObjects
+{
+    public class PersonSearchParameters : BaseObject
+    {
+        public PersonSearchParameters(Session session) : base(session)
+        {
+
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { SetPropertyValue("Name", ref name, value); }
+        }
+
+        private string userName;
+        public string UserName
+        {
+            get { return userName; }
+            set { SetPropertyValue("UserName", ref userName, value); }
+        }
+    }
+}
