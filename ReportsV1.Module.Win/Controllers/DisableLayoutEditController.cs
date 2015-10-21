8470ce4ea4738cef2df66950c946cab95ab7c1fe
Sergej Derjabkin <sergej@derjabkin.de>
2015-10-21 15:10:40 +0200

    Disable Layout Editor Controller

---
+++ ReportsV1.Module.Win/Controllers/DisableLayoutEditController.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Win.Layout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportsV1.Module.Win.Controllers
+{
+    public class DisableLayoutEditController : ViewController<DetailView>
+    {
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            XafLayoutControl layout = (XafLayoutControl)View.Control;
+            //layout.AllowCustomization = false;
+        }
+    }
+}
